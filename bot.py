47090c5ab7f98a7bdfc6444ef3a96ecd0f8a7b97
lgkpba <lgkpba@gmail.com>
2025-03-20 09:09:59 -0300

    esboço de menu

---
+++ bot.py
@@ -0,0 +1,10 @@
+def menu():
+
+    credenciais = {}
+
+    print("Primeiro vamos salvar suas credenciais:")
+    matricula = input("Digite sua matrícula: ")
+    cpf = input("Digite seu cpf:")
+    dataNascimento = input("Digite sua data de nascimento: ")
+
+menu()
