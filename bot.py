6bedc4eca2c9342425d797f9bb62a089643228e9
Lucas da Costa Rodrigues <70455992+lgkpba@users.noreply.github.com>
2025-03-20 11:03:04 -0300

    Brincando de fazer menu

--- bot.py
+++ bot.py
@@ -1,10 +1,21 @@
 def menu():
-
-    credenciais = {}
-
-    print("Primeiro vamos salvar suas credenciais:")
+    
+    print("----------------------------------------")
+    print("Primeiro vamos salvar suas informações!")
     matricula = input("Digite sua matrícula: ")
-    cpf = input("Digite seu cpf:")
-    dataNascimento = input("Digite sua data de nascimento: ")
-
-menu()
+    cpf = input("Digite seu cpf: ")
+    dataNascimento = input("Digite sua data de nascimento (dd/mm/aaa): ")
+    senha = input("Digite sua senha: ")
+    print("----------------------------------------")
+    materia = input("Em que matéria vc deseja se matricular?")
+    print("----------------------------------------")
+    print("Caso não deseje preencher algum(ns) dos campos a seguir, deixe-o(s) em branco")
+    professor = input("Informe o professor: ")
+    horario = input("Informe o horario (seguindo o padrão do sigaa): ")
+        
+    dadosPessoais = [matricula, cpf, dataNascimento, senha]
+    dadosMateria = [materia, (professor,horario)]
+    
+    return [dadosPessoais, dadosMateria]
+    
+print(menu()) 
